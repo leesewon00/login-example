d5e16f3b26475cacec27830fcd5567fe6024fe82
이세원 <107756067+leesewon00@users.noreply.github.com>
2024-01-04 13:30:05 +0900

    Delete src directory

--- src/main/java/loginexample/repository/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package loginexample.repository;
-
-import loginexample.entity.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, String> {
-}
