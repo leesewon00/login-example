3c2339c1ce622e157a70654e1d5e39805de4d073
이세원 <107756067+leesewon00@users.noreply.github.com>
2024-01-04 16:35:03 +0900

    Add files via upload

---
+++ src/main/java/com/tutorial/jwtsecurity/controller/MemberController.java
@@ -0,0 +1,25 @@
+package com.tutorial.jwtsecurity.controller;
+
+import com.tutorial.jwtsecurity.controller.dto.MemberResponseDto;
+import com.tutorial.jwtsecurity.service.MemberService;
+import com.tutorial.jwtsecurity.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/member")
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/me")
+    public ResponseEntity<MemberResponseDto> findMemberInfoById() {
+        return ResponseEntity.ok(memberService.findMemberInfoById(SecurityUtil.getCurrentMemberId()));
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<MemberResponseDto> findMemberInfoByEmail(@PathVariable String email) {
+        return ResponseEntity.ok(memberService.findMemberInfoByEmail(email));
+    }
+}
---
+++ src/main/java/com/tutorial/jwtsecurity/service/MemberService.java
@@ -0,0 +1,27 @@
+package com.tutorial.jwtsecurity.service;
+
+import com.tutorial.jwtsecurity.controller.dto.MemberResponseDto;
+import com.tutorial.jwtsecurity.repository.MemberRepository;
+import com.tutorial.jwtsecurity.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberResponseDto findMemberInfoById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .map(MemberResponseDto::of)
+                .orElseThrow(() -> new RuntimeException("로그인 유저 정보가 없습니다."));
+    }
+
+    public MemberResponseDto findMemberInfoByEmail(String email) {
+        return memberRepository.findByEmail(email)
+                .map(MemberResponseDto::of)
+                .orElseThrow(() -> new RuntimeException("유저 정보가 없습니다."));
+    }
+}
